b10f3632217b67246832a65a2626daebe720836e
Adrià Gomis <gomisroca@gmail.com>
2025-06-26 09:38:11 +0200

    🔨 add Seasonal and make Image, Description nullable

--- models/speisekarte.go
+++ models/speisekarte.go
@@ -3,9 +3,10 @@ package models
 type SpeisekarteItem struct {
 	ID   string    `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
-	Description string `json:"description" db:"description"`
+	Description *string `json:"description" db:"description"`
 	Categories []string `json:"categories" db:"categories"`
 	Tags []string `json:"tags" db:"tags"`
 	Price float32 `json:"price" db:"price"`
-	Image string `json:"image" db:"image"`
+	Image *string `json:"image" db:"image"`
+	Seasonal bool `json:"seasonal" db:"seasonal"`
 }
