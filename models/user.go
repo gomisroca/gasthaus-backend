f7ffb0b7513f90dad1a35dd5316aa1ea39ca08d6
Adrià Gomis <gomisroca@gmail.com>
2026-04-05 13:58:49 +0200

    feat: add createdAt

--- models/user.go
+++ models/user.go
@@ -1,7 +1,10 @@
 package models
 
+import "time"
+
 type User struct {
-	ID           string `json:"id" db:"id"`
-	Email        string `json:"email" db:"email"`
-	PasswordHash string `json:"password_hash" db:"password_hash"`
+    ID           string    `json:"id" db:"id"`
+    Email        string    `json:"email" db:"email"`
+    PasswordHash string    `json:"-" db:"password_hash"`
+    CreatedAt    time.Time `json:"created_at" db:"created_at"`
 }
